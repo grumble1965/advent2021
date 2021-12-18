92eb42ad1c15e9993b85a71031ef4e241170ea95
Kelly Hall <kmhall@gmail.com>
2021-12-18 09:46:08 -0800

    Day 18 challenge incomplete.
    Part a modified: explodable predicate now works on Pair data and
    not on a string representation.  Implemented the magnitude.

--- day18-a.py
+++ day18-a.py
@@ -31,16 +31,36 @@ class Pair:
             intint=lambda i, j: Pair.PAIRINT(Pair.INTINT(i//2, i - i//2), j) if i >= 10 else Pair.INTPAIR(i, Pair.INTINT(j//2, j - j//2)) if j >= 10 else Pair.INTINT(i-1, j-1),
             intpair=lambda i, q: (Pair.PAIRPAIR(Pair.INTINT(i//2, i-(i//2)), q) if i >= 10 else Pair.INTPAIR(i, q.split) if q.can_split else Pair.INTPAIR(i, q)),
             pairint=lambda p, j: (Pair.PAIRINT(p.split, j) if p.can_split else Pair.PAIRPAIR(p, Pair.INTINT(j//2, j-(j//2))) if j >= 10 else Pair.PAIRINT(p, j)),
-            pairpair=lambda p, q: (Pair.PAIRPAIR(p.split, q) if p.can_split else Pair.PAIRPAIR(p, q.split) if q.can_split else Pair.PAIRPAIR(p, q)))
+            pairpair=lambda p, q: (Pair.PAIRPAIR(p.split, q) if p.can_split else Pair.PAIRPAIR(p, q.split) if q.can_split else Pair.PAIRPAIR(p, q))
+        )
 
     @property
     def can_explode(self):
-        string = self.to_string
-        for idx in range(len(string)):
-            opens, closes = string[:idx].count('['), string[:idx].count(']')
-            if opens - closes >= 5:
-                return True
-        return False
+        # string = self.to_string
+        # for idx in range(len(string)):
+        #     opens, closes = string[:idx].count('['), string[:idx].count(']')
+        #     if opens - closes >= 5:
+        #         return True
+        # return False
+        return self.max_depth > 4
+
+    @property
+    def max_depth(self):
+        return self.match(
+            intint=lambda i, j: 1,
+            intpair=lambda i, q: 1 + q.max_depth,
+            pairint=lambda p, j: 1 + p.max_depth,
+            pairpair=lambda p, q: 1 + max([p.max_depth, q.max_depth])
+        )
+
+    @property
+    def magnitude(self):
+        return self.match(
+            intint=lambda i, j: 3*i + 2*j,
+            intpair=lambda i, q: 3*i + 2*q.magnitude,
+            pairint=lambda p, j: 3*p.magnitude + 2*j,
+            pairpair=lambda p, q: 3*p.magnitude + 2*q.magnitude
+        )
 
 
 def parse_string(string_input):
@@ -104,27 +124,30 @@ def main():
             if not line:
                 break
             tmp = line.strip()
-            print(f"{tmp}")
+            # print(f"{tmp}")
 
             pair = parse_string(tmp)
 
-            if snail_sum is None:
-                snail_sum = pair
-            else:
-                snail_sum = snail_addition(snail_sum, pair)
-            print("snail_sum = ", snail_sum.to_string,
-                  "splittable" if snail_sum.can_split else "not splittable", ',',
-                  "explodable" if snail_sum.can_explode else "not explodable")
+            # testing magnitude
+            print(f"magnitude {pair.to_string} = {pair.magnitude}")
 
-            foo = snail_sum
-            while foo.can_split:
-                foo = foo.split
-                print(foo.to_string)
+            # if snail_sum is None:
+            #     snail_sum = pair
+            # else:
+            #     snail_sum = snail_addition(snail_sum, pair)
+            # print("snail_sum = ", snail_sum.to_string,
+            #       "splittable" if snail_sum.can_split else "not splittable", ',',
+            #       "explodable" if snail_sum.can_explode else "not explodable")
+            #
+            # foo = snail_sum
+            # while foo.can_split:
+            #     foo = foo.split
+            #     print(foo.to_string)
 
         # print(snail_sum)
-        print(snail_sum.to_string)
-        foo = snail_reduce(snail_sum)
-        print(foo.to_string)
+        # print(snail_sum.to_string)
+        # foo = snail_reduce(snail_sum)
+        # print(foo.to_string)
 
 
 if __name__ == '__main__':
