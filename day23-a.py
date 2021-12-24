ef7886a94b0480ac8363d2ff2716cf14cb499648
Kelly Hall <kmhall@gmail.com>
2021-12-23 20:26:43 -0800

    Day 23 challenge ignored.
    I couldn't think of a good way to encode the game space let alone work
    on coding up the legal moves and writing a search algorithm.  Meh.

---
+++ day23-a.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit("Please provide a file name for input data")
+
+    filename = sys.argv[1]
+    with open(filename, "r") as inputfile:
+        while True:
+            line = inputfile.readline()
+            if not line:
+                break
+            tmp = line.strip()
+            print(tmp)
+
+
+if __name__ == '__main__':
+    main()
